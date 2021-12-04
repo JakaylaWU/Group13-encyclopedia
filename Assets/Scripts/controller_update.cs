c43b41f649d619e028a632b2cb6d21c59ad94079
Mokutan <839084866@qq.com>
2021-12-04 23:17:56 +0000

    Description UI Added
    Description UI Added

---
+++ Assets/Scripts/controller_update.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller_update : MonoBehaviour
+{
+    void Start()
+    {
+               
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/textOut.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class textOut : MonoBehaviour
+{
+    Text text;
+    string str;
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+        str = text.text;
+        text.text = "";
+        text.DOText(str, 10f);
+    }
+}
